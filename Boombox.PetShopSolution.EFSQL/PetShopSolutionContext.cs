ec531fadc3dd0d1b9084884d680471c59bccf516
Truncatedshark <56824915+Truncatedshark@users.noreply.github.com>
2021-09-22 10:15:27 +0200

    hahahah you looked xd

--- Boombox.PetShopSolution.EFSQL/PetShopSolutionContext.cs
+++ Boombox.PetShopSolution.EFSQL/PetShopSolutionContext.cs
@@ -22,11 +22,23 @@ namespace Boombox.PetShopSolution.EFSQL
         public DbSet<PetType> PetTypeTable { get; set; }
 
 
-        protected override void OnModelCreating(ModelBuilder modelbuilder)
-        
-        {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
 
+        {
+            modelBuilder.Entity<PetEntity>()
+                .HasOne(p => p.PetType)
+                .WithMany()
+                .HasForeignKey(p => new {TypeOdId = p.PetType});
+
+            modelBuilder.Entity<PetEntity>()
+                .HasData(new PetEntity {Id = 1, PetName = "Snoop Dogg", Price = 420});
             
+            modelBuilder.Entity<PetEntity>()
+                .HasData(new PetEntity {Id = 2, PetName = "Ice Cube", Price = 323});
+            
+            modelBuilder.Entity<PetEntity>()
+                .HasData(new PetEntity {Id = 3, PetName = "Dr. Dre", Price = 213});
+         
         }
         
 
